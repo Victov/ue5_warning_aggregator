4f77d5c4facdd9bd41eff89cfa44dd7c16834d8b
victov <victor.veldstra@gmail.com>
2023-01-19 21:19:09 +0100

    Make params runtime configurable

--- src/command_aggregate.rs
+++ src/command_aggregate.rs
@@ -5,23 +5,24 @@ use similar_string::find_best_similarity;
 use std::collections::HashMap;
 use std::path::PathBuf;
 
-pub(crate) async fn aggregate_log(path: &PathBuf, _with_warnings: bool) -> anyhow::Result<()> {
+pub(crate) async fn aggregate_log(
+    path: &PathBuf,
+    minimal_similarity: f32,
+    minimal_frequency: i32,
+) -> anyhow::Result<()> {
     let f = File::open(path).await?;
     let reader = BufReader::new(f);
     let lines = reader.lines();
 
-    const MIN_REQ_SIMILARITY: f64 = 0.50;
-    const MIN_REQ_COUNT: i32 = 1;
-
     let mut lines_with_warning = lines
         .filter_map(|a| a.ok())
-        .filter(|l: &String| l.contains("Warning:"));
+        .filter(|l: &String| l.contains("Warning: "));
 
     let mut occurrences_table: HashMap<String, i32> = HashMap::new();
     while let Some(line) = lines_with_warning.next().await {
         let keys: Vec<String> = occurrences_table.keys().cloned().collect();
         if let Some((existing_key, score)) = find_best_similarity(line.clone(), &keys) {
-            if score > MIN_REQ_SIMILARITY {
+            if score > minimal_similarity as f64 {
                 let count: &mut i32 = occurrences_table.get_mut(&existing_key).unwrap();
                 *count += 1;
             } else {
@@ -34,7 +35,7 @@ pub(crate) async fn aggregate_log(path: &PathBuf, _with_warnings: bool) -> anyho
 
     let mut results: Vec<(String, i32)> = occurrences_table
         .into_iter()
-        .filter(|(_, count)| *count > MIN_REQ_COUNT)
+        .filter(|(_, count)| *count > minimal_frequency)
         .collect();
     results.sort_by(|(_, count1), (_, count2)| count1.cmp(count2));
 
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,19 @@ async fn main() -> Result<()> {
         .subcommand(
             Command::new("aggregate")
                 .about("parse the log and aggregate contents")
-                .arg(arg!(-w --warnings "collect warnings").action(ArgAction::SetTrue))
+                .arg(
+                    arg!(-s --min_similarity "Required minimal warning similarity [0-100] percent")
+                    .action(ArgAction::Set)
+                    .default_value("80")
+                    .value_parser(clap::builder::RangedI64ValueParser::<i32>::new().range(0..100)),
+                )
+                .arg(
+                    arg!(-f --min_frequency "Required minimal frequency of warning before being reported")
+                    .action(ArgAction::Set)
+                    .default_value("1")
+                    .value_parser(clap::builder::RangedI64ValueParser::<i32>::new().range(0..100))
+                    )
+
                 .arg(
                     arg!(-l --logfile <FILE> "Path to logfile to operate on")
                         .required(true)
@@ -20,10 +32,16 @@ async fn main() -> Result<()> {
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("aggregate") {
-        let aggregate_warnings = *matches.get_one::<bool>("warnings").unwrap_or(&true);
+        let minimal_similarity = *matches
+            .get_one::<i32>("min_similarity")
+            .expect("should have default!");
+        let minimal_similarity: f32 = minimal_similarity as f32 / 100.0f32;
+        let minimal_frequency = *matches
+            .get_one::<i32>("min_frequency")
+            .expect("shoudl have default!");
+
         if let Some(path) = matches.get_one::<PathBuf>("logfile").as_ref() {
-            println!("going to aggregate: with warnigns: {}", aggregate_warnings);
-            command_aggregate::aggregate_log(path, aggregate_warnings).await?;
+            command_aggregate::aggregate_log(path, minimal_similarity, minimal_frequency).await?;
         } else {
             unreachable!("clap should enforce that there is always some path provided");
         }
